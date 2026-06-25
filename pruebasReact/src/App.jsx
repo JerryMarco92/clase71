fa520aab9b47a48b0e7de67fe8f0cda16975eb6a
Jerome <jercamseb@gmail.com>
2026-06-26 01:45:05 +0200

    probando tipos de componentes

---
+++ pruebasReact/src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import {Saludar, Suma, Gato} from "./components/Componentes";
+
+
+
+function App() {
+
+  const nombre = "Jerome";
+  const nuevo = false;
+
+
+
+  return (
+    <>
+      
+
+<Saludar />
+
+<p>Aqui sacamos lo guardado en la constante nombre que es : {nombre}</p>
+
+<Suma x={3} y={7}/>
+{Gato.map((expresion, index) => (
+  <li key={index}> {expresion} </li>
+))}
+
+<p>{!nuevo ? `Bienvenido ${nombre}` : "Registrate para acceder"}</p>
+
+    </>
+  )
+}
+
+export default App
---
+++ pruebasReact/src/components/Componentes.jsx
@@ -0,0 +1,14 @@
+export function Saludar(){
+    return (<>
+    
+        <h1>Hello World</h1>
+    
+    
+    </>);
+}
+
+export function Suma({ x, y }){
+    return <p>{x + y}</p>;
+}
+
+export const Gato = ["miau","prrrru prrruuu","meeeeeeeu","ppFFFFFFFFFFFFFF"];
